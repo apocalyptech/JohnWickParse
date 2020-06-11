57ba6dfa8603fc36b590458db3494bd5a1b44d23
CJ Kucera <pez@apocalyptech.com>
2020-06-11 11:49:27 -0500

    Bumping serialization version for Guardian Takedown patch

--- src/assets.rs
+++ src/assets.rs
@@ -29,7 +29,8 @@ mod meshes;
 //   v6: 2020-03-26 - DLC2
 //   v7: 2020-04-23 - Revenge of the Cartels and DLC2
 //   v8: 2020-05-13 - Pulled in some fresh commits from upstream
-const APOC_DATA_VER: u32 = 8;
+//   v9: 2020-06-11 - Guardian Takedown
+const APOC_DATA_VER: u32 = 9;
 
 pub use anims::{USkeleton, UAnimSequence, FTrack};
 pub use meshes::{USkeletalMesh, FMultisizeIndexContainer, FStaticMeshVertexDataTangent, FSkeletalMeshRenderData,
